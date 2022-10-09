fd720e7f75085f9edce7b37f80d16605f86f498d
Soumesh <iamsoumo26@gmail.com>
2022-10-09 13:56:42 +0530

    About , Alert named new components added and same changes has been made in navbar and textform , word count bug fixed and custom favicon added, dynamic title added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import "./App.css";
+import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
+import React,{useState} from "react";
 
 function App() {
+  const [mode,setMode]=useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert=(messege,type)=>{
+    setAlert(
+      {
+        msg:messege,
+        type:type
+      }
+    )
+    setTimeout(()=>{
+      setAlert(null)
+    },1500)
+  }
+
+  document.title=`TextUtils - ${mode.charAt(0).toUpperCase()+mode.slice(1).toLowerCase()} mode`
+  const toggleMode=()=>{
+    console.log("hey")
+    if(mode==="light")
+    {
+      setMode('dark')
+      document.body.classList.remove("bg-light")
+      document.body.classList.add("bg-dark")
+      document.body.style.color="white"
+      showAlert("Dark mode has been Enebled","success")
+    }
+    else{
+      setMode('light')
+      document.body.classList.remove("bg-dark")
+      document.body.classList.add("bg-light")
+      document.body.style.color="black"
+      showAlert("Light mode has been Enebled","success")
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About"/>{ /*it is called props like passing veriables to the html same as templates*/}
-      {/* <Navbar/>*/}
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />{ /*it is called props like passing veriables to the html same as templates*/}
+      <Alert alert={alert}/>
       <div className="container-fluid">
-      <Textform heading="Enter the Text To Analyze"/>
+      <Textform heading="Enter the Text To Analyze" mode={mode}/>
       </div>
+      {/* <About/> */}
     </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,38 +1,86 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 export default function Textform(props) {
-    const [text,setText]=useState('')
-    const handlerOnChange=(event)=>{
-        setText(event.target.value)
+  const [text, setText] = useState("");
+  const handlerOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const handlerUpClick = () => {
+    setText(text.toUpperCase());
+  };
+  const handlerLoClick = () => {
+    setText(text.toLowerCase());
+  };
+  const clearOnClick = () => {
+    setText("");
+  };
+  const noOfWords=(word)=>{
+    console.log(word.length)
+    if (word[word.length-1]==="" )
+    {
+      return word.length-1
     }
-    const handlerUpClick=()=>{
-        setText(text.toUpperCase())
+    else{
+      return word.length
     }
-    const handlerLoClick=()=>{
-        setText(text.toLowerCase())
-    }
-    const clearOnClick=()=>{
-        setText("")
-    }
-    let noOfWords=(text.length!==0)?text.split(" ").length:0
-    return (
-      <>
-        <h1 className='py-3'>{props.heading}</h1>
-        <div className="mb-3 ">
-            <textarea className="form-control" value={text}  onChange={handlerOnChange} id="myBox" rows="8"></textarea>
-            <div className="flex">
-            <button className="btn btn-primary my-3 mx-2" onClick={handlerUpClick}>Convet To UpperCase</button>
-            <button className="btn btn-primary my-3 mx-2" onClick={handlerLoClick}>Convet To LowerCase</button>
-            <button className="btn btn-primary my-3 mx-2" onClick={clearOnClick}>Clear</button>
-            </div>
+  }
+  const formatOnClick = () => {
+    let updatingText = text.split(" ").length !== 0 ? text.split(" ") : null;
+    let newText = updatingText
+      .map((word) => {
+        return (
+          word.substring(0, 1).toUpperCase() +
+          word.substring(1, word.length).toLowerCase()
+        );
+      })
+      .join(" ");
+    setText(newText);
+  };
+  return (
+    <>
+      <h1 className="py-3">{props.heading}</h1>
+      <div className="mb-3 ">
+        <textarea
+          className={`form-control bg-${props.mode} text-${props.mode === "light" ? "dark" : "light"}`}
+          value={text}
+          onChange={handlerOnChange}
+          id="myBox"
+          rows="8"
+        ></textarea>
+        <div className="flex">
+          <button
+            className="btn btn-primary my-3 mx-2"
+            onClick={handlerUpClick}
+          >
+            Convet To UpperCase
+          </button>
+          <button
+            className="btn btn-primary my-3 mx-2"
+            onClick={handlerLoClick}
+          >
+            Convet To LowerCase
+          </button>
+          <button className="btn btn-primary my-3 mx-2" onClick={clearOnClick}>
+            Clear
+          </button>
+          <button className="btn btn-primary my-3 mx-2" onClick={formatOnClick}>
+            Format
+          </button>
         </div>
-        <div className="container-fluid py-3">
-            <h2>Your Text Summery</h2>
-            <p>{noOfWords} word and {text.length} characters</p>
-            <p>{noOfWords*0.008} Minites to Read The Whole Sentence</p>
-            <h2>Preview</h2>
-            <p>{text}</p>
-        </div>
+      </div>
+      <div className="container-fluid py-3">
+        <h2>Your Text Summery</h2>
+        <p>
+          {noOfWords(text.split(" "))} word and{" "}
+          {text.length} characters
+        </p>
+        <p>
+          {noOfWords(text.split(" ")) * 0.008} Minites to
+          Read The Whole Sentence
+        </p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Write Something to Preview it here"}</p>
+      </div>
     </>
-    )
+  );
 }
