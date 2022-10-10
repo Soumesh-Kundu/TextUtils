04a87a666b7ee334721d15342cf68a69a7a5cc93
Soumesh <iamsoumo26@gmail.com>
2022-10-10 10:51:34 +0530

    Routes has been added some changes made in textform.js

--- src/App.js
+++ src/App.js
@@ -2,50 +2,57 @@ import "./App.css";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
-import React,{useState} from "react";
+import About from "./components/About";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
-  const [mode,setMode]=useState('light')
+  const [mode, setMode] = useState('light')
   const [alert, setAlert] = useState(null)
 
-  const showAlert=(messege,type)=>{
+  const showAlert = (messege, type) => {
     setAlert(
       {
-        msg:messege,
-        type:type
+        msg: messege,
+        type: type
       }
     )
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null)
-    },1500)
+    }, 1500)
   }
 
-  document.title=`TextUtils - ${mode.charAt(0).toUpperCase()+mode.slice(1).toLowerCase()} mode`
-  const toggleMode=()=>{
-    if(mode==="light")
-    {
+  document.title = `TextUtils - ${mode.charAt(0).toUpperCase() + mode.slice(1).toLowerCase()} mode`
+  const toggleMode = () => {
+    if (mode === "light") {
       setMode('dark')
       document.body.classList.remove("bg-light")
       document.body.classList.add("bg-dark")
-      document.body.style.color="white"
-      showAlert("Dark mode has been Enebled","success")
+      document.body.style.color = "white"
+      showAlert("Dark mode has been Enebled", "success")
     }
-    else{
+    else {
       setMode('light')
       document.body.classList.remove("bg-dark")
       document.body.classList.add("bg-light")
-      document.body.style.color="black"
-      showAlert("Light mode has been Enebled","success")
+      document.body.style.color = "black"
+      showAlert("Light mode has been Enebled", "success")
     }
   }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />{ /*it is called props like passing veriables to the html same as templates*/}
-      <Alert alert={alert}/>
-      <div className="container-fluid">
-        <Textform heading="Enter the Text To Analyze" mode={mode}/>
-      </div>
+      <Router>
+
+        <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />{ /*it is called props like passing veriables to the html same as templates*/}
+        <Alert alert={alert} />
+          <div className="container-fluid">
+        <Routes>
+            <Route path="/" element={<Textform heading="Enter the Text To Analyze" mode={mode} />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+          </div>
+      </Router>
     </>
   );
 }
